7cc8d0945d492d6dfe4098b921356eff227cb893
CarlosSalda <carlosjoelsalda@gmail.com>
2023-07-17 00:58:43 -0300

    small linter fixes

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -3,7 +3,7 @@ import { createStage } from '../gameHelpers';
 // Types
 import type { PLAYER } from './usePlayer';
 
-export type STAGECELL = [string | number, string];
+type STAGECELL = [string | number, string | number];
 export type STAGE = STAGECELL[][];
 
 export const useStage = (player: PLAYER, resetPlayer: () => void) => {
@@ -20,8 +20,6 @@ export const useStage = (player: PLAYER, resetPlayer: () => void) => {
         // If we don't find a 0 it means that the row is full and should be cleared
         if (row.findIndex(cell => cell[0] === 0) === -1) {
           setRowsCleared(prev => prev + 1);
-          // Create an empty row at the beginning of the array to push the Tetrominos down
-          // instead of returning the cleared row
           ack.unshift(new Array(newStage[0].length).fill([0, 'clear']) as STAGECELL[]);
           return ack;
         }
@@ -32,29 +30,29 @@ export const useStage = (player: PLAYER, resetPlayer: () => void) => {
     };
 
     const updateStage = (prevStage: STAGE): STAGE => {
-      // First flush the stage
-      // If it says "clear" but don't have a 0 it means that it's the players move and should be cleared
-      const newStage = prevStage.map(
-        row => row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
-      );
-
-      // Then draw the tetromino
-      player.tetromino.forEach((row, y) => {
-        row.forEach((value, x) => {
-          if (value !== 0) {
-            newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? 'merged' : 'clear'}`];
-          }
+        // First flush the stage
+        const newStage: STAGE = prevStage.map(row =>
+          row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+        );
+      
+        player.tetromino.forEach((row, y) => {
+          row.forEach((value, x) => {
+            if (value !== 0) {
+              newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? 'merged' : 'clear'}`];
+            }
+          });
         });
-      });
-
-      if (player.collided) {
-        resetPlayer();
-        return sweepRows(newStage);
-      }
-
-      return newStage;
-    };
-
+      
+        if (player.collided) {
+          resetPlayer();
+      
+          return sweepRows(newStage);
+        }
+      
+        return newStage;
+      };
+      
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     setStage(prev => updateStage(prev));
   }, [player.collided, player.pos?.x, player.pos?.y, player.tetromino, player.pos, resetPlayer]);
 
